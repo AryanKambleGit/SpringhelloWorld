3c27c3c8fc120f9de0a69a86d9cd481291bea3fa
Aryan Kamble <aryankamble41430@gmail.com>
2025-09-09 12:29:41 +0530

    Add files via upload
    📌 Today I Learned
    
    How to use ModelMapper to map between Entity and DTO classes easily.
    
    Using Postman to test APIs, especially with POST requests instead of only GET.
    
    The role of ResponseEntity in Spring Boot → to return not just data, but also status codes and metadata along with the response.

---
+++ MApperConfig.java
@@ -0,0 +1,16 @@
+package com.codingshuttle.youtube.Learning.Rest.APIs.confi;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MApperConfig {
+
+
+    @Bean
+    public ModelMapper maodelMapper(){
+        return new ModelMapper();
+
+    }
+}
